952b0ff5c748d17b19ce82a3b89a3d1673a6ad23
Sri Prasanna <k.sriprasanna@gmail.com>
2011-03-07 14:30:32 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComeTogetherFamily::Application.config.secret_token = '5c90dc3cfb15bc3a26aeab2d720cd9a2d75403d06fa455daea0c279e31cffb8e0c3f9af5529925e3b1085d5d079c4596a2d350f4d63e078cb98ddacd50bfd744'
